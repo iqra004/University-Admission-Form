e29387bd8db26145531fe25873a512dfffa2a790
usama <usamaaslam888@gmail.com>
2025-04-29 23:44:16 +0500

    changes

---
+++ script.js
@@ -0,0 +1,16 @@
+function confirmSubmission() {
+  const form = document.getElementById("admissionForm");
+  if (form.checkValidity()) {
+    const modal = new bootstrap.Modal(document.getElementById("confirmModal"));
+    modal.show();
+  } else {
+    form.reportValidity();
+  }
+}
+
+function submitForm() {
+  const fullName = document.getElementById("fullName").value;
+  alert(`Thank you, ${fullName}. Your application has been submitted successfully!`);
+  document.getElementById("admissionForm").reset();
+  bootstrap.Modal.getInstance(document.getElementById("confirmModal")).hide();
+}
